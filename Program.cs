1029f50abeff83d0d6f2abe569345c96d6bc7ccc
esta <esta20@gmail.com>
2020-05-21 09:41:45 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpCorePFCursus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Hal"
+                + "lo");
+        }
+    }
+}
