5aca0112fc3cc96dc01565b2b6c072ed04748f0b
esta <esta20@gmail.com>
2020-05-27 11:16:18 +0200

    MAI2020

---
+++ CSharpCorePFCursus/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharpCorePFCursus
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double straal = 10.5;
+            Console.WriteLine(Math.Pow(straal, 2) * Math.PI);
+
+
+        }
+    }
+}
